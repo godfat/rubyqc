d150d212f74edb05cc7e1b97d02f042ad43f5a25
Lin Jen-Shin <godfat@godfat.org>
2014-01-30 08:21:14 +0800

    instance level test

--- test/test_kind_of.rb
+++ test/test_kind_of.rb
@@ -7,4 +7,14 @@ describe 'kind_of' do
       check(klass){ |obj| obj.should.kind_of(klass) }
     end
   end
+
+  [[Fixnum], [Fixnum, Array]].each do |array|
+    should array.inspect do
+      check(array) do |a|
+        a.zip(array).each do |(instance, expected)|
+          instance.should.kind_of(expected)
+        end
+      end
+    end
+  end
 end
