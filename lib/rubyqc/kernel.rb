ddbe6a5221794b619d47782cf26c9d00fe193540
Lin Jen-Shin <godfat@godfat.org>
2014-01-30 08:00:37 +0800

    Class.rubyqc and Bignum.rubyqc

--- lib/rubyqc/kernel.rb
+++ lib/rubyqc/kernel.rb
@@ -2,11 +2,20 @@
 module RubyQC
   FixnumMax = 2 ** (0.size * 8 - 2) - 1
   FixnumMin = - FixnumMax - 1
+  FixnumSuc = FixnumMax + 1
+  BignumMax = FixnumMax * 10
+  BignumMin = FixnumMin * 10
 end
 
 module Kernel
   def rubyqc
     self
+  end
+end
+
+class Class
+  def rubyqc
+    new
   end
 end
 
@@ -19,5 +28,18 @@ end
 class Fixnum
   def self.rubyqc
     rand(RubyQC::FixnumMin..RubyQC::FixnumMax)
+  end
+end
+
+class Bignum
+  def self.rubyqc
+    case rand(2)
+    when 0
+      rand(RubyQC::BignumMin...RubyQC::FixnumMin)
+    when 1
+      rand(RubyQC::FixnumSuc...RubyQC::BignumMax)
+    else
+      raise "huh?"
+    end
   end
 end
