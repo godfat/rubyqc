5b001bc21db67d55cd2327f99248112ecd5d4c3d
Lin Jen-Shin <godfat@godfat.org>
2014-04-12 03:08:37 +0800

    Should from bacon won't work

--- test/test_api.rb
+++ test/test_api.rb
@@ -4,6 +4,7 @@ require 'rubyqc/test'
 describe RubyQC::API do
   should 'one_of' do
     check([Class]*5) do |klasses|
+      klasses -= [Should] # bacon's Should won't work...
       check(one_of(*klasses)) do |obj|
         klasses.find{ |klass| obj.kind_of?(klass) }.should.not.nil
       end
