be82b893ce940b9e1c2172932f3ec0dea3e2419f
Lin Jen-Shin <godfat@godfat.org>
2014-04-26 02:14:01 +0800

    move kind_of test to prelude

--- test/test_prelude.rb
+++ test/test_prelude.rb
@@ -11,4 +11,24 @@ describe 'prelude' do
       i.should.eq k
     end
   end
+
+  describe 'kind_of' do
+    [Fixnum, Bignum, Array, Integer, Class].each do |klass|
+      should klass.name do
+        check(klass){ |obj| obj.should.kind_of(klass) }
+      end
+    end
+
+    [[], [Fixnum], [Fixnum, Array],
+     {}, {:a => 0}, {'b' => 1}, {2 => Integer},
+     {nil => [Fixnum]}, {true => {false => [Bignum]}}
+    ].each do |spec|
+
+      should spec.inspect do
+        check(spec) do |generated|
+          verify_generated(generated, spec)
+        end
+      end
+    end
+  end
 end
