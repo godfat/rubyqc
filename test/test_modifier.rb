2ebc119683fd24a9cb8584ee0fc48bce92740a11
Lin Jen-Shin <godfat@godfat.org>
2014-04-26 10:36:12 +0800

    test for times

--- test/test_modifier.rb
+++ test/test_modifier.rb
@@ -2,6 +2,14 @@
 require 'rubyqc/test'
 
 describe RubyQC::Modifier do
+  should 'times' do
+    check(2..10) do |times|
+      t = 0
+      check.times(times){ t += 1 }
+      t.should.eq times
+    end
+  end
+
   should 'parallels' do
     check(5..11, 2..4).parallels(1) do |times, parallels|
       mutex = Mutex.new
