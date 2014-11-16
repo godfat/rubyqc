48e1fb84ff7a53ad508de1edd722c5359e22a017
Lin Jen-Shin <godfat@godfat.org>
2014-11-16 21:31:18 +0800

    eliminate warnings

--- lib/rubyqc.rb
+++ lib/rubyqc.rb
@@ -6,11 +6,11 @@ module RubyQC
   singleton_class.module_eval do
     attr_writer :default_times, :default_parallel
     def default_times
-      @default_times || 10
+      @default_times ||= 10
     end
 
     def default_parallel
-      @default_parallel || 1
+      @default_parallel ||= 1
     end
   end
 end
